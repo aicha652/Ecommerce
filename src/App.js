52bd91b165508bc481a39cb1246d8ae73552d79a
aicha652 <aichaazlf652@gmail.com>
2024-07-26 16:55:55 +0200

    data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import Home from './pages/HomePage/Home';
-import Shop from './pages/ShopPage/Shop';
-import Cart from './pages/CartPage/Cart';
-import Header from './common/Header';
-import Footer from './common/Footer';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-        <Footer />
-      </Router>
+    <div> 
     </div>
   );
 }
